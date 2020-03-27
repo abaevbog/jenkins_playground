396d06d67683bfe0fffe9d6276260287557b1d4b
Bogdan Abaev <abaevbog@grinnell.edu>
2020-03-27 10:23:37 -0700

    weewew

--- server.py
+++ server.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     name = request.args.get("name", "World")
-    return f'Hello once again, {escape(name)}!'
+    return f'Hello once again kuku, {escape(name)}!'
